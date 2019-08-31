cbb0547573eaa4772713fd330cc94fa31d6b5781
Demian <demian@presstronic.com>
2019-08-30 20:16:08 -0400

    Adding more page setup for login

--- src/App.js
+++ src/App.js
@@ -5,9 +5,36 @@ function App() {
   return (
     <div className="App">
       <nav>Some navigation buttons</nav>
-      <main>The content</main>
-      <sidebar>Testing Side</sidebar>
-      <footer>Copyrights and stuff</footer>      
+      <div className="main-wrapper">
+        <div className="main">The content</div>
+
+        <div className="sidebar">
+          <div className="login-form">
+            <h1>Login here</h1>
+            <form className="login" action="" method="post"> 
+              <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" />  
+              </div>
+              <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" />
+              </div>  
+
+              <div className="actions">
+                <input type="submit" name="login" value="Login"/> <a href="/forgot">I forgot my password</a>
+              </div>
+            </form>
+
+            <div class="account">
+              <p>Create a new account <a href="">here</a>.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>      
+      <footer>Copyrights and stuff</footer>
     </div>
 
   );
